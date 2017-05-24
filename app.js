1ff11525ed30a666e3cabe18ca86e77a9026814e
Christoffer Olsson <Christoffer.Olsson@ne.se>
2017-05-24 15:21:04 +0200

    Added chosen-angular

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express()
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use('/static/angular', express.static(path.join(__dirname, 'node_modules/angular')));
+app.use('/static/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/static/chosen-js', express.static(path.join(__dirname, 'node_modules/chosen-js')));
+app.use('/static/angular-chosen', express.static(path.join(__dirname, 'node_modules/angular-chosen/dist')));
 
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
