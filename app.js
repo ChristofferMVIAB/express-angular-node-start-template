b140e8222503e9ec4e9e864014dc8d6929092865
Christoffer Olsson <Christoffer.Olsson@ne.se>
2017-05-24 15:01:31 +0200

    Added loading of angular and basic example

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path')
 var app = express()
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/static/angular', express.static(path.join(__dirname, 'node_modules/angular')));
+
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
 });
